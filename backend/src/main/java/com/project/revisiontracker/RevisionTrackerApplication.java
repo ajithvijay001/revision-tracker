09d9ef812d1abba3a20cd95aa09f6605048a4db4
Ajith <ajith007.v@gmail.com>
2025-07-11 00:20:38 +0530

    chore: organized projects into backend & frontend

---
+++ backend/src/main/java/com/project/revisiontracker/RevisionTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.project.revisiontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/revisiontracker/RevisionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.revisiontracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
